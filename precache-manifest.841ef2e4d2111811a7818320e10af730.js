c2e7bfa0c992e9f657fa1c2756347d6938e8de3c
pyaenaing <43054337+pyaenaing@users.noreply.github.com>
2020-06-04 18:45:50 -0700

    Updates

---
+++ precache-manifest.841ef2e4d2111811a7818320e10af730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207c64b457138748ac36b2788af44e91",
+    "url": "/Note-App/index.html"
+  },
+  {
+    "revision": "b92cca810f92b6a5063c",
+    "url": "/Note-App/static/css/main.66d80578.chunk.css"
+  },
+  {
+    "revision": "be5de111a03f7559787f",
+    "url": "/Note-App/static/js/2.7ca85e7f.chunk.js"
+  },
+  {
+    "revision": "b92cca810f92b6a5063c",
+    "url": "/Note-App/static/js/main.ad13c2f4.chunk.js"
+  },
+  {
+    "revision": "bd65de24ec0c68e1246d",
+    "url": "/Note-App/static/js/runtime-main.d36590b3.js"
+  }
+]);
